9679a52c8423f79c856f9eab1a564564a2de1b4c
Namig <namig.pashayev1@gmail.com>
2026-02-02 16:48:17 +0400

    Create Planet component

---
+++ src/app/roadmap/[map]/components/Planet.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import styles from './Planet.module.scss';
+import Image from 'next/image';
+import { PlanetData } from '@/lib/types/planet';
+
+interface Props {
+  map: string;
+  planet: PlanetData;
+}
+
+const Planet = ({ map, planet }: Props) => {
+  return (
+    <section className={styles.planetSection}>
+      <div className={styles.planetCnr}>
+        <Link
+          scroll={false}
+          href={`${map}/${planet.id}`}
+          className={styles.link}
+        >
+          <Image
+            src={planet.imageUrl}
+            alt={planet.name}
+            width={1024}
+            height={1024}
+            className={styles.planet}
+            loading="lazy"
+          />
+        </Link>
+        <div className={styles.orbit}>
+          {planet.tags[0] && (
+            <div className={styles.satellite}>{planet.tags[0]}</div>
+          )}
+          {planet.tags[1] && (
+            <div className={styles.satellite}>{planet.tags[1]}</div>
+          )}
+          {planet.tags[2] && (
+            <div className={styles.satellite}>{planet.tags[2]}</div>
+          )}
+          {planet.tags[3] && (
+            <div className={styles.satellite}>{planet.tags[3]}</div>
+          )}
+        </div>
+
+        <div className={styles.outerOrbit}></div>
+      </div>
+    </section>
+  );
+};
+
+export default Planet;
