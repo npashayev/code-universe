dcecbf7c0d41ae6b1d23608cea4258f4e6795bed
Namig <namig.pashayev1@gmail.com>
2026-02-02 16:49:29 +0400

    Create ContentList component

---
+++ src/app/roadmap/[map]/[planetId]/components/ContentList.tsx
@@ -0,0 +1,19 @@
+import { PlanetContent } from '@/lib/types/planet';
+import Content from './contents/Content';
+
+interface Props {
+  contents: PlanetContent[];
+}
+
+const ContentList = ({ contents }: Props) => {
+  return (
+    <section>
+      <h2>Tasks</h2>
+      {contents.map(content => (
+        <Content key={content.id} content={content} />
+      ))}
+    </section>
+  );
+};
+
+export default ContentList;
