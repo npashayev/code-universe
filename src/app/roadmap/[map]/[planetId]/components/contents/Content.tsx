04cb7c1087f0b8b1e2fb504e00a9e4113263bbd2
Namig <namig.pashayev1@gmail.com>
2026-02-02 16:51:37 +0400

    Create content component

---
+++ src/app/roadmap/[map]/[planetId]/components/contents/Content.tsx
@@ -0,0 +1,45 @@
+import { PlanetContent } from '@/lib/types/planet';
+import CodeExampleBlock from './CodeExampleBlock';
+import HtmlElementBlock from './HtmlElementBlock';
+
+// import TextContentBlock from './TextContentBlock';
+// import ImplementationTaskBlock from './ImplementationTaskBlock';
+// import HtmlElementBlock from './HtmlElementBlock';
+// import CodeTaskBlock from './CodeTaskBlock';
+// import ImageBlock from './ImageBlock';
+
+interface Props {
+  content: PlanetContent;
+}
+
+const Content = ({ content }: Props) => {
+  switch (content.type) {
+    // case 'text':
+    // case 'h2':
+    // case 'h3':
+    // case 'h4':
+    // case 'h5':
+    // case 'h6':
+    //   return <TextContentBlock content={content} />;
+
+    // case 'implementation-task':
+    //   return <ImplementationTaskBlock content={content} />;
+
+    // case 'code-example':
+    //   return <CodeExampleBlock content={content} />;
+
+    // case 'html-element':
+    //   return <HtmlElementBlock content={content} />;
+
+    // case 'code-task':
+    //   return <CodeTaskBlock content={content} />;
+
+    // case 'image':
+    //   return <ImageBlock content={content} />;
+
+    default:
+      return null;
+  }
+};
+
+export default Content;
