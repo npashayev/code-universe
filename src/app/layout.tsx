bf9b511f5e411e1931f5a86b566fd0bc4eda90fd
Namig <namig.pashayev1@gmail.com>
2026-02-13 17:11:58 +0400

    Uncomment Navbar in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${nunito.className} antialiased`}>
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
       </body>
     </html>
