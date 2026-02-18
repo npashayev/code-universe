1784795f8e7d4d2cd939cb9abf023df72a10dd0d
Namig <namig.pashayev1@gmail.com>
2026-02-18 10:49:01 +0400

    Update PlanetClient component to accept partial CreatePlanetData as well

--- src/app/roadmap/[map]/[planetId]/components/PlanetClient.tsx
+++ src/app/roadmap/[map]/[planetId]/components/PlanetClient.tsx
@@ -1,4 +1,8 @@
-import { PlanetData, SupportedLanguage } from '@/types/planet';
+import {
+  CreatePlanetData,
+  PlanetData,
+  SupportedLanguage,
+} from '@/types/planet';
 import PlanetHeader from './PlanetHeader';
 import ResearchTopics from './ResearchTopics';
 import Resources from './Resources';
@@ -6,24 +10,38 @@ import ContentList from './ContentList';
 import Questions from './contents/Questions';
 
 interface Props {
-  planet: PlanetData;
+  planet: PlanetData | Partial<CreatePlanetData>;
   locale: SupportedLanguage;
 }
 
 const PlanetClient = ({ planet, locale }: Props) => {
   const { image, localized } = planet;
+
+  if (!localized?.[locale]) {
+    return (
+      <main className="page text-center p-20 text-2xl">
+        No content available for preview
+      </main>
+    );
+  }
+
   const localizedData = localized[locale];
+
   return (
     <main className="page pt-36 px-16 pb-14 text-[18px] space-y-8">
       <PlanetHeader
-        localizedData={planet.localized[locale]}
+        localizedData={localized[locale]}
         image={{
-          url: image.url,
-          metadata: image.metadata,
-          alt: image.alt[locale],
+          url: image?.url || '',
+          metadata: image?.metadata || { width: 0, height: 0 },
+          alt: image?.alt[locale] || '',
         }}
       />
-      <ResearchTopics researchTopics={localizedData.researchTopics} />
+
+      {localizedData.researchTopics &&
+        localizedData.researchTopics.length > 0 && (
+          <ResearchTopics researchTopics={localizedData.researchTopics} />
+        )}
       {localizedData.resources && localizedData.resources.length > 0 && (
         <Resources resources={localizedData.resources} />
       )}
