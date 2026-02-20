e261501b0c070c48287b994d3af00cfef577d856
Namig <namig.pashayev1@gmail.com>
2026-02-20 09:42:52 +0400

    Minor changes

--- src/components/providers/QueryProvider.tsx
+++ src/components/providers/QueryProvider.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
-export default function QueryProvider({ children }: { children: ReactNode }) {
+interface Props {
+  children: ReactNode;
+}
+
+const QueryProvider = ({ children }: Props) => {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
-}
+};
+
+export default QueryProvider;
