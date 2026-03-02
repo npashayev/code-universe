5a8995edc6356cd9739dfb188d383cd540702103
Namig <namig.pashayev1@gmail.com>
2026-03-02 12:42:37 +0400

    Add remote image pattern configuration to next.config.ts for external image support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-12991a9f3b9e43eba46833e1ac879e50.r2.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
