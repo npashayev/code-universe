4b390584ae7d02a61c56a3fec688de56c0aa56cf
VasimB <vbayramov4@std.beu.edu.az>
2026-03-08 22:26:16 +0400

    Refactor HomePage layout and update content for improved clarity and engagement

--- src/app/(public)/[locale]/page.tsx
+++ src/app/(public)/[locale]/page.tsx
@@ -5,34 +5,29 @@ import SplashCursor from '@/components/react-bits/SplashCursor';
 
 export default function HomePage() {
   return (
-    <>
-      <main className="min-h-screen flex items-center justify-center gap-52 text-white px-40 py-28 pl-64">
-        <SolarSystem />
-        <div>
-          <div className="flex items-center justify-center gap-8">
-            <Image
-              className="h-32 w-auto select-none"
-              src={galaxy}
-              alt="galaxy icon"
-              width={512}
-              height={512}
-            />
-            <h1 className="text-6xl leading-tight">
-              Explore the Universe of Web Development
-            </h1>
-          </div>
-          <p className="text-3xl mt-12">
-            Start your journey through the cosmos of code — one planet at a
-            time.
-          </p>
-          <p className="text-3xl mt-7">
-            Learn the building blocks of the web — HTML, CSS, and JavaScript —
-            through interactive planets, visual guides, and hands-on examples
-            made for absolute beginners.
-          </p>
+    <main className="min-h-screen flex items-center justify-center gap-52 text-white px-40 py-28 pl-64">
+      <SolarSystem />
+      <div>
+        <div className="flex items-center justify-center gap-8">
+          <Image
+            className="h-32 w-auto select-none"
+            src={galaxy}
+            alt="galaxy icon"
+            width={512}
+            height={512}
+          />
+          <h1 className="text-6xl leading-tight">
+            Explore the Universe of Code
+          </h1>
         </div>
-      </main>
-      {/* <SplashCursor /> */}
-    </>
+        <p className="text-3xl mt-12">
+          Move beyond tutorials: implement industry-standard solutions within a professional environment.
+        </p>
+        <p className="text-3xl mt-7">
+          Learn the foundational principles of programming through industry standard tasks and practical examples built for the modern workplace.
+        </p>
+      </div>
+      <SplashCursor />
+    </main>
   );
 }
