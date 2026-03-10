5093c05d7c622b221817ca41be84c9bca9e701ce
Namig <namig.pashayev1@gmail.com>
2026-03-10 15:59:12 +0400

    Update prop types for PlanetClient

--- src/app/(public)/[locale]/roadmap/[map]/[planetId]/components/PlanetClient.tsx
+++ src/app/(public)/[locale]/roadmap/[map]/[planetId]/components/PlanetClient.tsx
@@ -3,10 +3,10 @@ import ResearchTopics from './ResearchTopics';
 import Resources from './Resources';
 import ContentList from './ContentList';
 import Questions from './contents/Questions';
-import { GetPlanetResponse } from '@/lib/planet/getPlanet';
+import { PublicPlanetResponse } from '@/lib/planet/getPlanet';
 
 interface Props {
-  planet: GetPlanetResponse
+  planet: PublicPlanetResponse;
 }
 
 const PlanetClient = ({ planet }: Props) => {
@@ -16,10 +16,7 @@ const PlanetClient = ({ planet }: Props) => {
   return (
     <div className="planet-detail-page">
       <main className="page pt-36 px-16 pb-14 text-[18px] text-slate-200 space-y-8">
-        <PlanetHeader
-          localizedData={localized}
-          image={image}
-        />
+        <PlanetHeader localizedData={localized} image={image} />
 
         {researchTopics && researchTopics.length > 0 && (
           <ResearchTopics researchTopics={researchTopics} />
@@ -27,9 +24,7 @@ const PlanetClient = ({ planet }: Props) => {
         {resources && resources.length > 0 && (
           <Resources resources={resources} />
         )}
-        {contents && contents.length > 0 && (
-          <ContentList contents={contents} />
-        )}
+        {contents && contents.length > 0 && <ContentList contents={contents} />}
         {questions && questions.length > 0 && (
           <Questions questions={questions} />
         )}
