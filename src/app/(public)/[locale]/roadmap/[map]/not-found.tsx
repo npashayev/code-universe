9816f41c03149c39edd36d7a0404944e820d6899
Namig <namig.pashayev1@gmail.com>
2026-03-10 15:58:43 +0400

    Refactor NotFound page to use NotFoundContent component for better code reuse and add a specific NotFound page for planets

--- src/app/(public)/[locale]/roadmap/[map]/not-found.tsx
+++ src/app/(public)/[locale]/roadmap/[map]/not-found.tsx
@@ -1,5 +1,7 @@
 import NotFoundContent from '@/components/feedback/NotFoundContent';
 
 export default function NotFound() {
-  return <NotFoundContent />;
+  return (
+    <NotFoundContent text="The category you're looking for doesn't exist." />
+  );
 }
