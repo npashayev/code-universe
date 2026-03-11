5b06f718aac202c4c2e81e19b78b215a376520d5
Namig <namig.pashayev1@gmail.com>
2026-03-12 01:08:26 +0400

    Enhance PlanetClient component with navigation and admin controls, adding back link and conditional rendering for delete and update actions

--- src/app/(public)/[locale]/roadmap/[category]/[planetId]/components/PlanetClient.tsx
+++ src/app/(public)/[locale]/roadmap/[category]/[planetId]/components/PlanetClient.tsx
@@ -5,17 +5,37 @@ import ContentList from './ContentList';
 import Questions from './contents/Questions';
 import { PublicPlanetResponse } from '@/lib/planet/getPlanet';
 import PlanetNavigation from './PlanetNavigation';
+import { Link } from '@/lib/next-intl/navigation';
+import UpdatePlanetLink from '@/components/admin/UpdatePlanetLink';
+import DeletePlanetButtonClient from './DeletePlanetButtonClient';
+import PrivateComponent from '@/components/shared/PrivateComponent';
 
 interface Props {
   planet: PublicPlanetResponse;
 }
 
 const PlanetClient = ({ planet }: Props) => {
-  const { category, image, localized } = planet;
+  const { id, category, image, localized } = planet;
   const { researchTopics, resources, contents, questions } = localized;
 
   return (
     <main className="page px-30 text-lg pt-36 pb-16 space-y-8 bg-linear-to-br from-slate-950 via-slate-900 to-slate-950">
+      <div className='fixed z-1 top-6 left-0 px-30 flex justify-between w-full items-center'>
+        <Link
+          className='px-3 py-1 rounded bg-slate-800 border border-white/10 hover:bg-slate-900'
+          href={`/roadmap/${category}`}
+        >
+          Go back
+        </Link>
+
+        <PrivateComponent roles={["ADMIN"]}>
+          <div className='flex gap-2 items-center bg-slate-950 px-2 py-1 rounded-lg border border-white/10'>
+            <DeletePlanetButtonClient planetId={id} category={category} />
+            <UpdatePlanetLink planetId={id} />
+          </div>
+        </PrivateComponent>
+      </div>
+
       <PlanetHeader
         category={category}
         localizedData={localized}
