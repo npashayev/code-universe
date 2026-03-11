fc9acd9f563204da3b5d983a0cd656d20240cdd2
Namig <namig.pashayev1@gmail.com>
2026-03-12 00:58:53 +0400

    Add DeletePlanetButton and UpdatePlanetLink components for planet management

---
+++ src/components/admin/DeletePlanetButton.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useDeletePlanet } from '@/lib/hooks/queries/usePlanet';
+import Dialog from '../ui/Dialog'
+import RemoveButton from './RemoveButton'
+import { useState } from 'react';
+
+interface Props {
+    planetId: string;
+    onSuccess?: () => void;
+}
+
+const DeletePlanetButton = ({ planetId, onSuccess }: Props) => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const { mutate: deletePlanet, isPending } = useDeletePlanet(onSuccess);
+    return (
+        <>
+            {modalOpen && (
+                <Dialog
+                    icon="warning"
+                    title="Delete Planet"
+                    body="Are you sure you want to delete this planet?"
+                    onConfirm={() => deletePlanet(planetId)}
+                    onClose={() => setModalOpen(false)}
+                />
+            )}
+            <RemoveButton
+                isPending={isPending}
+                onClick={() => setModalOpen(true)}
+            />
+        </>
+
+    )
+}
+
+export default DeletePlanetButton
