87b483555765e9591d2f0fa1c485ddb08b50ad6d
Namig <namig.pashayev1@gmail.com>
2026-03-12 00:59:01 +0400

    Refactor useDeletePlanet hook to call onSuccess callback after successful deletion

--- src/lib/hooks/queries/usePlanet.ts
+++ src/lib/hooks/queries/usePlanet.ts
@@ -1,13 +1,17 @@
 'use client';
 
-import { deletePlanet, updatePlanetList } from '@/app/actions/planet';
+import { deletePlanet } from '@/app/actions/deletePlanet';
+import { updatePlanetList } from '@/app/actions/planet';
 import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 
-export const useDeletePlanet = () =>
+export const useDeletePlanet = (onSuccess?: () => void) =>
   useMutation({
     mutationFn: deletePlanet,
-    onSuccess: () => toast.success('Planet deleted successfully'),
+    onSuccess: () => {
+      toast.success('Planet deleted successfully');
+      onSuccess?.();
+    },
     onError: err => toast.error(err.message),
   });
 
