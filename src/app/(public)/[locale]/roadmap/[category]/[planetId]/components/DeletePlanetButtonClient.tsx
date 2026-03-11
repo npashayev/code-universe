04623a416e3e8f926dc200f4b128b2f3e83ad72e
Namig <namig.pashayev1@gmail.com>
2026-03-12 00:59:37 +0400

    Add DeletePlanetButtonClient

---
+++ src/app/(public)/[locale]/roadmap/[category]/[planetId]/components/DeletePlanetButtonClient.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import DeletePlanetButton from "@/components/admin/DeletePlanetButton"
+import { useRouter } from "@/lib/next-intl/navigation";
+import { PlanetCategory } from "@/types/planet";
+
+interface Props {
+    planetId: string;
+    category: PlanetCategory;
+}
+
+const DeletePlanetButtonClient = ({ planetId, category }: Props) => {
+    const router = useRouter();
+    return <DeletePlanetButton
+        planetId={planetId}
+        onSuccess={() => router.push(`/roadmap/${category}`)}
+    />
+}
+
+export default DeletePlanetButtonClient
