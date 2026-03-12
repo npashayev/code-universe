32bbe17490d9c6485b61c59d9b83fe32f0f475e7
Namig <namig.pashayev1@gmail.com>
2026-03-12 12:04:21 +0400

    Replace UpdatePlanetLink with UpdateLink component for improved reusability and cleaner code structure.

---
+++ src/components/admin/UpdateLink.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/lib/utils/cn';
+import { Edit } from 'lucide-react';
+import Link from 'next/link';
+import { ComponentPropsWithoutRef } from 'react';
+
+type Props = ComponentPropsWithoutRef<typeof Link>;
+
+const UpdateLink = ({ className, ...props }: Props) => {
+    return (
+        <Link
+            className={cn(
+                "p-2 text-slate-400 hover:text-red-400 hover:bg-red-400/15 rounded-lg transition-all",
+                className
+            )}
+            target="_blank"
+            rel="noreferrer"
+            {...props}
+        >
+            <Edit size={18} />
+        </Link>
+    );
+};
+
+export default UpdateLink;
