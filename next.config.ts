6e14b3907adf29c15c12416c4bf6362a23202628
Namig <namig.pashayev1@gmail.com>
2026-03-20 01:25:06 +0400

    Set cacheComponents to true in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: true,
   reactCompiler: true,
   images: {
     remotePatterns: [
