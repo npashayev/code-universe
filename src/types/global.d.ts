62dbfd9f1546e754d34b6427549ea95db6790be6
Namig <namig.pashayev1@gmail.com>
2026-03-22 18:28:45 +0400

    Refactor TypeScript type declarations: remove next-env.d.ts, update global.d.ts, and create static-images.d.ts for image module types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,15 +1,3 @@
-import type { StaticImageData } from 'next/image';
-
 declare module '*.css';
 declare module '*.scss';
 declare module '*.sass';
-
-declare module '*.png' {
-  const content: StaticImageData;
-  export default content;
-}
-
-declare module '*.webp' {
-  const content: StaticImageData;
-  export default content;
-}
