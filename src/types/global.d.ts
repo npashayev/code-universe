5905af1186634e22d6591c32227274bf41c30dab
Namig <namig.pashayev1@gmail.com>
2026-03-22 18:20:17 +0400

    Update TypeScript configuration and extend global type declarations for image modules

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,15 @@
+import type { StaticImageData } from 'next/image';
+
 declare module '*.css';
 declare module '*.scss';
 declare module '*.sass';
+
+declare module '*.png' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: StaticImageData;
+  export default content;
+}
